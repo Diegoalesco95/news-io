f5fb9468c23509917c1ec728b26aaec4ff08cf0c
Diegoalesco95 <42728227+Diegoalesco95@users.noreply.github.com>
2022-06-22 14:44:50 -0500

    :tada: Initial configuration

--- src/app/pages/tab3/tab3.module.ts
+++ src/app/pages/tab3/tab3.module.ts
@@ -4,7 +4,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { Tab3Page } from './tab3.page';
-import { ExploreContainerComponentModule } from '../explore-container/explore-container.module';
+import { ExploreContainerComponentModule } from 'src/app/explore-container/explore-container.module';
 
 import { Tab3PageRoutingModule } from './tab3-routing.module';
 
@@ -17,6 +17,6 @@ import { Tab3PageRoutingModule } from './tab3-routing.module';
     RouterModule.forChild([{ path: '', component: Tab3Page }]),
     Tab3PageRoutingModule,
   ],
-  declarations: [Tab3Page]
+  declarations: [Tab3Page],
 })
 export class Tab3PageModule {}
