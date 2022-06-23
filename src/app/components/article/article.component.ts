a357df1f60bd258b82e9aebeeda022457f33d0b2
Diegoalesco95 <42728227+Diegoalesco95@users.noreply.github.com>
2022-06-23 15:05:01 -0500

    :sparkles: Add Action sheet to card menu

--- src/app/components/article/article.component.ts
+++ src/app/components/article/article.component.ts
@@ -1,5 +1,5 @@
 import { Component, Input } from '@angular/core';
-import { Platform } from '@ionic/angular';
+import { ActionSheetController, Platform } from '@ionic/angular';
 import { InAppBrowser } from '@awesome-cordova-plugins/in-app-browser/ngx';
 
 import { Article } from 'src/app/interfaces';
@@ -13,14 +13,51 @@ export class ArticleComponent {
   @Input() article: Article;
   @Input() index: number;
 
-  constructor(private iab: InAppBrowser, private platform: Platform) {}
-
-  onClick() {}
+  constructor(
+    private iab: InAppBrowser,
+    private platform: Platform,
+    private actionsSheetCtrl: ActionSheetController
+  ) {}
 
   openArticle() {
     if (this.platform.is('ios') || this.platform.is('android')) {
       const browser = this.iab.create(this.article.url);
       browser.show();
     }
+    window.open(this.article.url, '_blank');
+  }
+
+  async openOptions() {
+    const actionSheet = await this.actionsSheetCtrl.create({
+      header: 'Options',
+      buttons: [
+        {
+          text: 'Share',
+          icon: 'share-outline',
+          handler: () => this.onShareArticle(),
+        },
+        {
+          text: 'Favorite',
+          icon: 'heart-outline',
+          handler: () => this.onToggleFavorite(),
+        },
+        {
+          text: 'Cancel',
+          icon: 'close-outline',
+          role: 'cancel',
+          cssClass: 'text-red',
+        },
+      ],
+    });
+
+    await actionSheet.present();
+  }
+
+  onToggleFavorite() {
+    console.log('Favorite');
+  }
+
+  onShareArticle() {
+    console.log('Share article');
   }
 }
